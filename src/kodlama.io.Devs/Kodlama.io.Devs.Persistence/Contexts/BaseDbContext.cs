ead5c4e58008f6a1db5d8fced563f5ff5cfaaf94
Satılmış TUNÇ <tunc0687@hotmail.com>
2022-09-02 23:15:35 +0300

    Add Repository, add  migration

---
+++ src/kodlama.io.Devs/Kodlama.io.Devs.Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Kodlama.io.Devs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+
+        public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProgrammingLanguage>(a =>
+            {
+                a.ToTable("ProgrammingLanguages").HasKey(k => k.Id);
+                a.Property(a => a.Id).HasColumnName("Id");
+                a.Property(a => a.Name).HasColumnName("Name");
+            });
+
+            ProgrammingLanguage[] programmingLanguageEntitySeed = { new(1, "C#"), new(2, "Python") };
+            modelBuilder.Entity<ProgrammingLanguage>().HasData(programmingLanguageEntitySeed);
+        }
+    }
+}
