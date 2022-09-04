ba116010743b11bd49c0ebf3f928814ee033c32d
Satılmış TUNÇ <tunc0687@hotmail.com>
2022-09-04 13:29:06 +0300

    Add CQRS Created Command

---
+++ src/kodlama.io.Devs/Kodlama.io.Devs.Application/Features/ProgrammingLanguages/Rules/ProgrammingLanguageBusinessRules.cs
@@ -0,0 +1,28 @@
+﻿using Core.CrossCuttingConcerns.Exceptions;
+using Kodlama.io.Devs.Application.Services.Repositories;
+using Kodlama.io.Devs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodlama.io.Devs.Application.Features.ProgrammingLanguages.Rules
+{
+    public class ProgrammingLanguageBusinessRules
+    {
+        private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+
+        public ProgrammingLanguageBusinessRules(IProgrammingLanguageRepository programmingLanguageRepository)
+        {
+            _programmingLanguageRepository = programmingLanguageRepository;
+        }
+
+        public async Task ProgrammingLanguageNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            ProgrammingLanguage? result = await _programmingLanguageRepository.GetAsync(x => x.Name == name);
+
+            if (result == null) throw new BusinessException("ProgrammingLanguage name exists.");
+        }
+    }
+}
